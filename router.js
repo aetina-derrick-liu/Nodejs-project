34ba19a5f415de72c11ef31373562ccefd008c2b
derrick <derrick_liu@aetina.com>
2022-07-18 14:47:19 +0800

    upload file finished

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-function route(handle,pathname,response) {
+function route(handle,pathname,response,request) {
     console.log("router.js get:",pathname);   
     if (typeof handle[pathname] === 'function'){
-        handle[pathname](response);
+        handle[pathname](response,request);
     }else{
         console.log("This type is not defined");
         response.writeHead(404, {"Content-Type": "text/plain"});
